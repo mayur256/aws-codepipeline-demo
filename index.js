93585dc7957c7c23d06902a0d55ecbea1e1274f0
Mayur Upadhayay <mayur123a@gmail.com>
2024-03-27 16:21:23 +0530

    [chore]: Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("node:http");
+const fs = require("fs/promises");
+
+const PORT = 8001;
+
+// create a local server to receive data from
+const server = http.createServer((_, response) => {
+    // send the index file
+    (async(response) => {
+        const data = await fs.readFile(__dirname + '/index.html', { encoding: 'utf8' });
+        response.writeHead(200, {
+            'Content-Length': Buffer.byteLength(data),
+            'Content-Type': 'text/html'
+        }).end(data);
+    })(response);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port : ${PORT}`);
+});
